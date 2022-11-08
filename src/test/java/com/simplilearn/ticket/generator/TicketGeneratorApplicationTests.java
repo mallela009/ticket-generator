9acb2ff0cbffcbdaa510e6f71c176c3bc76d36d7
mallela009 <mallelaarjuna009@gmail.com>
2022-11-08 16:43:14 +0530

    first commit

---
+++ src/test/java/com/simplilearn/ticket/generator/TicketGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ticket.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
